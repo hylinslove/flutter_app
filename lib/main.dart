b98644999b91ce76886b8131699635d168f55853
meng <763577618@qq.com>
2019-08-03 17:50:01 +0800

    ListView基本用法

--- lib/main.dart
+++ lib/main.dart
@@ -67,29 +67,43 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              child: Text("控件"),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) => WidgetPage()
-                ));
-
-              }
-            )
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
+        body:
+//      Center(
+//        // Center is a layout widget. It takes a single child and positions it
+//        // in the middle of the parent.
+//
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: <Widget>[
+//            RaisedButton(
+//              child: Text("控件"),
+//              onPressed: () {
+//                Navigator.push(context, MaterialPageRoute(
+//                  builder: (context) => WidgetPage()
+//                ));
+//
+//              }
+//            ),
+            ListView(
+          padding: EdgeInsets.all(20),
+          children: <Widget>[
+            ListTile(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => WidgetPage()));
+              },
+              leading: Icon(Icons.widgets),
+              title: Text("控件"),
+            ),
+          ],
+        )
+//          ],
+//        ),
 //        child: Column(
 //          // Column is also layout widget. It takes a list of children and
 //          // arranges them vertically. By default, it sizes itself to fit its
@@ -116,8 +130,7 @@ class _MyHomePageState extends State<MyHomePage> {
 //            ),
 //          ],
 //        ),
-      ),
-
-    );
+//      ),
+        );
   }
 }
