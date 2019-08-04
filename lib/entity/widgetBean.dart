151fcc709b7195fe3952e1e69c25fe35008ebdd1
meng <763577618@qq.com>
2019-08-04 18:27:06 +0800

    list view builder，实现长列表

---
+++ lib/entity/widgetBean.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WidgetBean  {
+  String name;
+  String description;
+  IconData icon;
+
+  WidgetBean({this.name, this.description, this.icon});
+
+
+}
--- lib/page/WidgetPage.dart
+++ lib/page/WidgetPage.dart
@@ -1,26 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/entity/widgetBean.dart';
 
 class WidgetPage extends StatefulWidget {
+
+
   @override
   WidgetPageState createState() => WidgetPageState();
 }
 
 class WidgetPageState extends State<WidgetPage> {
+  List<WidgetBean> widgets;
   @override
   Widget build(BuildContext context) {
+    initWidgets();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Widget"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('控件页面')
-
-          ],
-        ),
+      /**
+       * long list
+       */
+      body: ListView.builder(
+        padding: EdgeInsets.all(2),
+        itemCount: widgets.length,
+        itemBuilder: (context,index) {
+          return Container(
+            alignment: Alignment.topLeft,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.amber,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(3))
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "${widgets[index].name}",
+                  style: TextStyle(
+                    fontSize: 26,
+                    color: Colors.blueAccent,
+                    fontWeight:FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  "${widgets[index].description}",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                    fontStyle: FontStyle.italic
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
+  }
+
+  void initWidgets() {
+    widgets = new List();
+    widgets.add(WidgetBean(
+      name: "TextView",
+      description: "文本控件",
+      icon: Icons.text_fields
+    ));
+    widgets.add(WidgetBean(
+        name: "TextView",
+        description: "文本控件",
+        icon: Icons.text_fields
+    ));
+    widgets.add(WidgetBean(
+        name: "TextView",
+        description: "文本控件",
+        icon: Icons.text_fields
+    ));
+    widgets.add(WidgetBean(
+        name: "TextView",
+        description: "文本控件",
+        icon: Icons.text_fields
+    ));
+    widgets.add(WidgetBean(
+        name: "TextView",
+        description: "文本控件",
+        icon: Icons.text_fields
+    ));
+
   }
 }
