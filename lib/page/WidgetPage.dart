8d2cdd047e3b941ee86026a5fd0ec7e28e67d8fd
meng <763577618@qq.com>
2019-08-05 20:52:53 +0800

    list view 修改样式，增加点击事件

--- lib/page/WidgetPage.dart
+++ lib/page/WidgetPage.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/entity/widgetBean.dart';
 
 class WidgetPage extends StatefulWidget {
-
-
   @override
   WidgetPageState createState() => WidgetPageState();
 }
 
 class WidgetPageState extends State<WidgetPage> {
   List<WidgetBean> widgets;
+
   @override
   Widget build(BuildContext context) {
     initWidgets();
 
     return Scaffold(
+      backgroundColor: Color(0xFFEEEEEE),
       appBar: AppBar(
         title: Text("Widget"),
       ),
@@ -22,40 +22,44 @@ class WidgetPageState extends State<WidgetPage> {
        * long list
        */
       body: ListView.builder(
-        padding: EdgeInsets.all(2),
+        padding: EdgeInsets.all(10),
         itemCount: widgets.length,
-        itemBuilder: (context,index) {
-          return Container(
-            alignment: Alignment.topLeft,
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.amber,
-                width: 1,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              print("item clicked");
+            },
+            child: Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 0,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(7))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "${widgets[index].name}",
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Text(
+                    "${widgets[index].description}",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey,
+                        fontStyle: FontStyle.italic),
+                  ),
+                ],
               ),
-              borderRadius: BorderRadius.all(Radius.circular(3))
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  "${widgets[index].name}",
-                  style: TextStyle(
-                    fontSize: 26,
-                    color: Colors.blueAccent,
-                    fontWeight:FontWeight.w400,
-                  ),
-                ),
-                Text(
-                  "${widgets[index].description}",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.grey,
-                    fontStyle: FontStyle.italic
-                  ),
-                ),
-              ],
             ),
           );
         },
@@ -66,30 +70,14 @@ class WidgetPageState extends State<WidgetPage> {
   void initWidgets() {
     widgets = new List();
     widgets.add(WidgetBean(
-      name: "TextView",
-      description: "文本控件",
-      icon: Icons.text_fields
-    ));
+        name: "TextView", description: "文本控件", icon: Icons.text_fields));
     widgets.add(WidgetBean(
-        name: "TextView",
-        description: "文本控件",
-        icon: Icons.text_fields
-    ));
+        name: "TextView", description: "文本控件", icon: Icons.text_fields));
     widgets.add(WidgetBean(
-        name: "TextView",
-        description: "文本控件",
-        icon: Icons.text_fields
-    ));
+        name: "TextView", description: "文本控件", icon: Icons.text_fields));
     widgets.add(WidgetBean(
-        name: "TextView",
-        description: "文本控件",
-        icon: Icons.text_fields
-    ));
+        name: "TextView", description: "文本控件", icon: Icons.text_fields));
     widgets.add(WidgetBean(
-        name: "TextView",
-        description: "文本控件",
-        icon: Icons.text_fields
-    ));
-
+        name: "TextView", description: "文本控件", icon: Icons.text_fields));
   }
 }
